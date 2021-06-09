552e03dee5138733317fb2d2c22a5f5c5fdca392
tales.ferreira <tales.ferreira@targetwork.com>
2021-06-09 08:49:58 -0400

    Implementado novo projeto para desligamento e reinicializaçao do dispositivo via API

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { exec } = require('child_process')
+
+const app = express()
+
+app.get("/poweroff", () => exec("poweroff"))
+
+app.get("/reboot", () => exec("reboot"))
+
+app.listen(3000)
